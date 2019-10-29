9c4297e1ce443b91796d8ed3f930f988aaaab0fa
juan pita <sala@HOR-SALA501-002.unihorizonte.salas>
2019-10-29 15:52:34 -0500

    mi primer commit:

---
+++ Senasalud2/src/senasalud2/Senasalud2.java
@@ -0,0 +1,26 @@
+
+package senasalud2;
+
+import java.util.Scanner;
+
+
+
+
+
+public class Senasalud2 {
+
+    
+    public static void main(String[] args) {
+        
+    String nombre;
+    Scanner nomb= new Scanner (System.in);
+        System.out.println("Digite su nombre");
+        nombre=nomb.next();
+        System.out.println("Su nombre es:"+""+nombre);
+    
+        
+        
+    
+    }
+    
+}
